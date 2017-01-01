120dca07fa912e485cb451c340d96efbc0cdb88c
cfrancobedoya <cfrancobedoya@gmail.com>
2017-01-01 11:41:35 -0500

    Terminada clase 18, clousures javascript

---
+++ clase_18-CLOSURES-/index.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+const config = require('./config');
+
+app.listen(config.port , function (){
+    console.log(`Aplicación corriendo en htpp://localhost:${config.port}`)
+    console.log("Desarrollado por: Cristian Franco Bedoya");
+    console.log("Franco Software 2016");
+});
